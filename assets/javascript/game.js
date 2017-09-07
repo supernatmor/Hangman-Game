407bf5c55085be12155fd0c7a0edce905ac5830f
Nate Morgan <natmor88@gmail.com>
2017-09-06 22:20:07 -0400

    event listener added

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//creating array of possible words to guess
+var bank = ["yoda", "deathstar", "lightsaber", "luke", "lei", "han", "chewbacca", "c3p0", "r2d2", "stormtrooper", "alderaan", "tatooine", "endor"];
+//convert array to uppercase, log to verify
+for (var i =0, i < bank.length;  i++) {
+	bank[i] = bank[i].toUpperCase();
+	console.log( bank[i] ) ;
+}
+
+
