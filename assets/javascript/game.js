0292a1a50d4bf923b676dd73acf3eb41bdf76f99
Nate Morgan <natmor88@gmail.com>
2017-09-16 11:50:06 -0400

    it works now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,91 @@
-var bigText = document.getElementById("user-Text");
-var guesses= document.getElementById("guesses");
+var bank = ["yoda", "luke", "lei", "han", "chewbacca", "lightsaber", "stormtrooper", "deathstar", "tattooine", "alderaan"];
+var guessBox = document.getElementById("word");
+var letterBox = document.getElementById("guesses");
+var winBox = document.getElementById("wins");
+var lossBox = document.getElementById("losses");
+var blanksArray, rand, noDec, guess, missed, right, match, noPrev, letter, guessed;
+var wins = 0;
+var losses = 0;
 
-//creating array of possible words to guess
-var bank = ["yoda", "deathstar", "lightsaber", "luke", "lei", "han", "chewbacca", "c3p0", "r2d2", "stormtrooper", "alderaan", "tatooine", "endor"];
 
-//convert array to uppercase, log to verify
-for (var i =0; i < bank.length;  i++) {
-	bank[i] = bank[i].toUpperCase();
-	//console.log( bank[i] ) ;
+
+function reset () {
+	rand = bank.length * Math.random();
+	noDec = Math.floor (rand);
+	guess = bank[noDec].split(""); //word to be guessed
+	missed = 0;
+	right = 0;
+	match = false;
+	noPrev = true;  
+	letter = "";
+	guessed = "";
+	blanksArray = [];
+	for (var i = 0; i < guess.length; i++){
+	blanksArray[i] = "_";
+	}
+	setBlanks();
+	winBox.textContent = wins;
+	lossBox.textContent = losses; 
+	
 }
 
-//setting the word to be guessed and console for debug
-var rand = bank.length * Math.random();
-var noDec = Math.floor (rand);
-var guess = bank[noDec].split(""); //word to be guessed
-console.log(guess);
-var missed = 0;
-var right = 0;
-
-//setting number of "_" to be displayed and sending to "word" id
-var gLength = guess.length;
-var blanksArray = [];  //created array so can compare user input later
-for (var c = 0;  c  < gLength; c++) {
-	blanksArray[c]="_ ";
-	//console.log(blanksArray);
-}
-
-
-//displays # of "_" matching length of word to be guessed
 function setBlanks() {
 	var blanks = blanksArray.join(" ");
-	//console.log(blanks);
-	bigText.textContent = blanks;
+	guessBox.textContent = blanks;
 }
 
 
-//set variable and tracking for user input
-var letter = "";
-document.onkeypress = function(event) {
-letter = event.key.toUpperCase();
-checkLetter();
-//console.log(letter)
-var letters = "";
-letters = letters +  " " + event.key;
-guesses.textContent + letters;
+reset();
 
+
+function prevCheck() {
+	for (var i = 0; i < guessed.length; i++) {
+		if (letter === guessed[i]) {
+			noPrev = false; 
+				
+		}
+	}
 }
 
-
-
-//compare user input to computer word, change corresponding _ on screen
-function checkLetter() {
-	var match = false; 
-	for (var count = 0; count < gLength; count++) {
-		if (letter === guess[count]) {
-			//alert("you got one right");
-			blanksArray[count] = letter;
-			match = true;
-			right++;
+function check() {
+	match = false;
+	for (var i = 0; i < guess.length; i ++) {
+		if (letter === guess[i]) {
+				blanksArray[i] = letter;
+				match = true; 
+				right ++
 		}
 	}
 	setBlanks();
 	if (match == false) {
-		missed++;
+		missed ++; 
 	}
-	//console.log (match, missed);
 
-
-	//win and loss conditions
 	if (missed >=5) {
-	alert ("you lose");
+		alert("you lose");
+		losses++;
+		reset();
 	}
-	else if ( right >= gLength) {
-		alert("you win");
+	else if (right == guess.length) {
+		alert("You Win!!");
+		wins++;
+		reset();
 	}
 
+	
 }
 
-
-
-
-
-
-
+document.onkeypress = function(event) {
+	letter = event.key.toLowerCase();
+	prevCheck();
+	if (noPrev == true) {
+		check();
+		guessed += letter;
+		letterBox.textContent = guessed;
+	}
+	else {
+		alert("you have already guessed that letter")
+		noPrev= true;
+	}
+	
+}
